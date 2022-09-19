01ac56daf33ab2f98df91dd945bb28375340776c
Andrey <shuzlik@mail.ru>
2022-09-19 19:41:52 +0300

    homework 6

---
+++ Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+Console.Write("Введите количество вводимых элементов: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArray(int size1)
+{
+    int[] array = new int[size1];
+    for (int i = 0; i < size1; i++)
+    {
+        Console.Write("Введите число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        array[i] = a;
+    }
+    return array;
+}
+int PositiveNumber(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) result++;
+    }
+    return result;
+}
+int[] arr = CreateArray(size);
+int result = PositiveNumber(arr);
+Console.WriteLine($"Количество положительных чисел: {result}");
---
+++ Task43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+
+Console.WriteLine("Введите координаты первой прямой");
+Console.Write("k: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй прямой");
+Console.Write("k: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+void CheckingLines(double a1, double b1, double a2, double b2)
+{
+    if ((k1 == k2) && (b1 == b2))
+        Console.WriteLine("Прямые совпадают");
+    else if (k1 == k2)
+        Console.WriteLine("Прямые параллельны");
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+        Console.WriteLine($"x = {x}, y = {y}");
+    }
+}
+CheckingLines(k1, b1, k2, b2);

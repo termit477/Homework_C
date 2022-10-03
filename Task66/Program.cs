2e2427abe607d015d88c3632ecdd809f337c79c8
Andrey <shuzlik@mail.ru>
2022-10-03 15:46:19 +0300

    homework 9

---
+++ Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+
+int SummMToN(int num1, int num2)
+{
+    summ += num1;
+    if (num1 < num2) SummMToN(num1 + 1, num2);
+    else if (num1 > num2) SummMToN(num1 - 1, num2);
+    return summ;
+}
+
+int sum = SummMToN(m, n);
+Console.WriteLine($"Сумма элементов от M до N равна {sum}");
---
+++ Task68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int FuncAccerman(int num1, int num2)
+{
+    if (num1 == 0) return num2 + 1;
+    if ((num1 != 0) && (num2 == 0)) return FuncAccerman(num1 - 1, 1);
+    if ((num1 > 0) && (num2 > 0)) return FuncAccerman(num1 - 1, FuncAccerman(num1, num2 - 1));
+    return FuncAccerman(num1, num2);
+}
+int accerman = FuncAccerman(m, n);
+Console.WriteLine(accerman);
